ed7887e61a3ae389f927751614b0a79365efcc75
Vimala Moger <vimalamoger@gmail.com>
2024-08-15 18:29:31 -0500

    adding a folder:

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var server = express();
+var routes = require('./routes/route');
+
+var mongoose = require('mongoose');
+const cors = require('cors');
+
+mongoose.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true, useUnifiedTopology:true},function checkDB(error){
+    if(error){
+        console.log("db error");
+    }else{
+        console.log("db connected");
+    }
+}); 
+ server.use(cors());
+server.use(express.json());
+server.use(routes); 
+
+
+server.listen(8000, function check(error){
+    if(error){
+        console.log("error");
+    }else{
+        console.log("started");
+    }
+}); 
